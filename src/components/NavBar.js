f5bc9fc120d5af49a7600aad366992865fb9a711
Lanny MacMillan <LannyMacMillan@gmail.com>
2022-08-13 14:50:27 -0400

    margin to about section divs

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,16 +8,19 @@ import Navbar from 'react-bootstrap/Navbar';
 const NavBar = () => {
     return (
         <>
-        <Navbar bg="dark" variant="dark">
+        <Navbar collapseOnSelect fixed='top' expand='sm' bg="dark" variant="dark">
             <Container>
-                <Navbar.Brand href={'/Admin'}>BZL&L Bandpage</Navbar.Brand>
-                    <Nav className="mr-auto">
-                        <Nav.Link href={'/'}>Home</Nav.Link>
-                        <Nav.Link href={'/Shows'}>Shows</Nav.Link>
-                        <Nav.Link href={'/About'}>About</Nav.Link>
-                        <Nav.Link href={'/Music'}>Music</Nav.Link>
-                        <Nav.Link href={'/Gallery'}>Gallery</Nav.Link>
-                    </Nav>
+                <Navbar.Toggle aria-controls='responsive-navbar-nav' />
+                <Navbar.Collapse id='responsive-navbar-nav'>
+                    <Navbar.Brand href={'/Admin'}>BZL&L Bandpage</Navbar.Brand>
+                        <Nav className="mr-auto">
+                            <Nav.Link href={'/'}>Home</Nav.Link>
+                            <Nav.Link href={'/Shows'}>Shows</Nav.Link>
+                            <Nav.Link href={'/About'}>About</Nav.Link>
+                            <Nav.Link href={'/Music'}>Music</Nav.Link>
+                            <Nav.Link href={'/Gallery'}>Gallery</Nav.Link>
+                        </Nav>
+                </Navbar.Collapse>
             </Container>
         </Navbar>
 
