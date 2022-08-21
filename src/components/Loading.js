4f9694c63c8ee9f77f29b5f09a032cf182fd4ea3
Lanny MacMillan <LannyMacMillan@gmail.com>
2022-08-20 21:47:28 -0400

    comment out loading component for heroku issue

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,76 +1,76 @@
-import {useState } from 'react'
-import '../App.css'
-import { Audio } from  'react-loader-spinner'
-import { ThreeDots } from  'react-loader-spinner'
-import { TailSpin } from  'react-loader-spinner'
-import { Rings } from  'react-loader-spinner'
-import { Puff } from  'react-loader-spinner'
-import { Oval } from  'react-loader-spinner'
-import { Hearts } from  'react-loader-spinner'
-import { Grid } from  'react-loader-spinner'
-import { Circles } from  'react-loader-spinner'
-import { Bars } from  'react-loader-spinner'
-import { BallTriangle } from  'react-loader-spinner'
+// import {useState } from 'react'
+// import '../App.css'
+// import { Audio } from  'react-loader-spinner'
+// import { ThreeDots } from  'react-loader-spinner'
+// import { TailSpin } from  'react-loader-spinner'
+// import { Rings } from  'react-loader-spinner'
+// import { Puff } from  'react-loader-spinner'
+// import { Oval } from  'react-loader-spinner'
+// import { Hearts } from  'react-loader-spinner'
+// import { Grid } from  'react-loader-spinner'
+// import { Circles } from  'react-loader-spinner'
+// import { Bars } from  'react-loader-spinner'
+// import { BallTriangle } from  'react-loader-spinner'
 
 
-const Loading = () => {
-    const [loading, setLoading] = useState(false)
+// const Loading = () => {
+//     const [loading, setLoading] = useState(false)
 
-    // const onClick = async () => {
-    //     setLoading(true);
-    //     await fetch('/').then(() => {
-    //         //handle data
-    //     })
-    //     setLoading(false)
-    // }
-
-
+//     // const onClick = async () => {
+//     //     setLoading(true);
+//     //     await fetch('/').then(() => {
+//     //         //handle data
+//     //     })
+//     //     setLoading(false)
+//     // }
 
 
-    return (
-        <>
 
-        <div className='mainContainer'>
-            <div className='Music'>
-            <Audio color="#00BFFF" height={80} width={80} />
-            <br/>
-            <ThreeDots color="#00BFFF" height={80} width={80} />
-            <br/>
-            <TailSpin color="#00BFFF" height={80} width={80} />
-            <br/>
-            <Rings color="#00BFFF" height={80} width={80} />
-            <br/>
-            <Puff color="#00BFFF" height={80} width={80} />
-            <br/>
-            <Oval color="#00BFFF" height={80} width={80} />
-            <br/>
-            <Hearts color="#00BFFF" height={80} width={80} />
-            <br/>
-            <Grid color="#00BFFF" height={80} width={80} />
-            <br/>
-            <Circles color="#00BFFF" height={80} width={80}/>
-            <br/>
-            <Bars color="#00BFFF" height={80} width={80} />
-            <br/>
-            <BallTriangle color="#00BFFF" height={80} width={80} />
-            </div>
-        </div>
-            {loading ? (
-                <Audio
-                height = "80"
-                width = "80"
-                radius = "9"
-                color = 'green'
-                ariaLabel = 'three-dots-loading'     
-                wrapperStyle
-                wrapperClass
-                />
-            ):(
-                null
-            )}
+
+//     return (
+//         <>
+
+//         <div className='mainContainer'>
+//             <div className='Music'>
+//             <Audio color="#00BFFF" height={80} width={80} />
+//             <br/>
+//             <ThreeDots color="#00BFFF" height={80} width={80} />
+//             <br/>
+//             <TailSpin color="#00BFFF" height={80} width={80} />
+//             <br/>
+//             <Rings color="#00BFFF" height={80} width={80} />
+//             <br/>
+//             <Puff color="#00BFFF" height={80} width={80} />
+//             <br/>
+//             <Oval color="#00BFFF" height={80} width={80} />
+//             <br/>
+//             <Hearts color="#00BFFF" height={80} width={80} />
+//             <br/>
+//             <Grid color="#00BFFF" height={80} width={80} />
+//             <br/>
+//             <Circles color="#00BFFF" height={80} width={80}/>
+//             <br/>
+//             <Bars color="#00BFFF" height={80} width={80} />
+//             <br/>
+//             <BallTriangle color="#00BFFF" height={80} width={80} />
+//             </div>
+//         </div>
+//             {loading ? (
+//                 <Audio
+//                 height = "80"
+//                 width = "80"
+//                 radius = "9"
+//                 color = 'green'
+//                 ariaLabel = 'three-dots-loading'     
+//                 wrapperStyle
+//                 wrapperClass
+//                 />
+//             ):(
+//                 null
+//             )}
 
         
-        </>
-    )
-}
-export default Loading
+//         </>
+//     )
+// }
+// export default Loading
